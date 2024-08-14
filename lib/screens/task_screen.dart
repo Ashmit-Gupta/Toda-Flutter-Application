4c647a859454b941a94a831947aff3069cb37c27
Ashmit Gupta <ashmitgupta.ann05@gmail.com>
2024-08-15 01:01:12 +0530

    created the task screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -7,7 +7,63 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Column(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 35,
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  EdgeInsets.only(top: 30, right: 30, bottom: 30, left: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      size: 80,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 50,
+                  ),
+                  SizedBox(width: 0, height: 15),
+                  Text(
+                    "Todoey",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    '12 tasks',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
